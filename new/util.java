fbcee17829efde40fc6f1270aae96376ed94a050
lucifer206516 <bharth0@lucifer@gmail.com>
2025-03-13 15:20:39 +0530

    age

---
+++ new/util.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class util{
+    public static void main(String args[]){
+        Scanner  input = new Scanner(System.in);
+        System.out.println("enter your age");
+        int age = input.nextInt();
+        System.out.println(age);
+    
+    }
+}

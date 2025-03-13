0e0b5be60fa7f7fbf2646ecd895bc41af320f289
lucifer206516 <bharth0@lucifer@gmail.com>
2025-03-13 11:11:43 +0530

    IfelseCon

---
+++ demo/IfelseCon.java
@@ -0,0 +1,11 @@
+public class IfelseCon{
+    public static void main(String args[]){
+        int no = 6;
+        if(no%2==0){
+            System.out.println("The number is even");
+        }
+        else{
+            System.out.println("The number is even");
+        }
+    }
+}

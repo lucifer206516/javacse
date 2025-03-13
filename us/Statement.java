50de7b5cc3068967123f1df3526fe0e900e5d97a
lucifer206516 <bharth0@lucifer@gmail.com>
2025-03-13 13:30:29 +0530

    state

---
+++ us/Statement.java
@@ -0,0 +1,12 @@
+public class Statement{
+    public static void main(String[] args) {
+        boolean a=true;
+        boolean b= true;
+        if (a=true) {
+            System.out.println("the a value is true.");
+        }
+        if(b=true){
+            System.out.println("the b value is true.");
+        }
+    }
+}

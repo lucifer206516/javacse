8c842a52cc9c7947bd7397b0e3ff777f3f3b2614
lucifer206516 <bharth0@lucifer@gmail.com>
2025-03-13 10:53:28 +0530

    Cond

---
+++ Cond/Conditional.java
@@ -0,0 +1,12 @@
+public class Conditional{
+     public static void main(String[] args){
+         System.out.println(10<2);
+         System.out.println(10>2);
+         System.out.println(10<=2);
+         System.out.println(10>=2);
+         System.out.println(10!=2);
+         System.out.println(10==2);
+ 
+ 
+     }
+ }

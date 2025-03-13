170c89cc1f2957d22863bb395d345217ec659d7b
lucifer206516 <bharth0@lucifer@gmail.com>
2025-03-13 14:31:14 +0530

    single

---
+++ loop/loop.java
@@ -0,0 +1,12 @@
+
+public class loop{
+    public static void main(String args[]){
+        String[] arr = {"red","blue","green"};
+        for(String ball : arr){
+            if(ball.equals("red") ){
+                System.out.println("picked red ball");
+            }
+            break;
+        }
+    }
+}

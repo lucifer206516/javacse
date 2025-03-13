bf64991d96b3b5c9353914a055c51a9af8971c51
lucifer206516 <bharth0@lucifer@gmail.com>
2025-03-13 11:35:41 +0530

    Major

---
+++ mager/Major.java
@@ -0,0 +1,11 @@
+public class Major {
+    public static void main(String args[]){
+        int age = 19;
+        if(age>=18){
+            System.out.println("You are Major");
+        }
+        else{
+            System.out.println("You are not major");
+        }
+        }
+    }

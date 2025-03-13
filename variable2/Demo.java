c40c84d662c434a90d20f3bb3d75e57402bb8da6
lucifer206516 <bharth0@lucifer@gmail.com>
2025-03-13 10:08:47 +0530

    boolean

--- variable2/Demo.java
+++ variable2/Demo.java
@@ -1,6 +1,6 @@
 public class Demo{
     public static void main(String args[]){
         boolean isValid =false;
-        System.out.println("isValid");
+        System.out.println(isValid);
     }
 }

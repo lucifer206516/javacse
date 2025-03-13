ed82eb5750e5161a1516d47ec643e989ddda26e7
lucifer206516 <bharth0@lucifer@gmail.com>
2025-03-13 12:07:12 +0530

    male

---
+++ user/male.java
@@ -0,0 +1,16 @@
+ import java.util.*;
+public class  male{
+    public static void main(String args[]){
+        int no = 5;
+        Scanner inp = new Scanner(System.in);
+        System.out.println("Enter Your Gender: ");
+        String n = inp.nextLine();
+        String un = n.toUpperCase();
+        if(un.equals("M")){
+            System.out.println("HELLO SIR, PLEASE ENTER YOUR NAME: ");
+        }
+        else{
+            System.out.println("HELLO MAM, PLEASE ENTER YOUR NAME ");
+        }
+    }
+}

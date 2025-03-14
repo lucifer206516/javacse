1056cfc87da5bfb7e464876786978b62d6b7f40f
lucifer206516 <bharth0@lucifer@gmail.com>
2025-03-14 10:29:22 +0530

    while7

--- is/while7.java
+++ is/while7.java
@@ -1,11 +1,12 @@
+import java.util.*;
 public class while7{
-    public static void main(String args[]){
-        int i = 1;
-        int value =0;
-        while(i<10){
-            i=i+1;
-            value = 7*i;
-            System.out.println(value);
+    public static void main(String args[]) {
+        int i=1;
+        int num = 7;
+        while (i<= 10) {
+            System.out.println(num + " x " + i + " = " + (num* i));
+            i= i+1; 
         }
     }
 }
+

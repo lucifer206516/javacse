26fe2fdd11861077015e339f426647af85d4dee8
lucifer206516 <bharth0@lucifer@gmail.com>
2025-03-14 10:18:04 +0530

    while7

---
+++ is/while7.java
@@ -0,0 +1,11 @@
+public class while7{
+    public static void main(String args[]){
+        int i = 1;
+        int value =0;
+        while(i<10){
+            i=i+1;
+            value = 7*i;
+            System.out.println(value);
+        }
+    }
+}

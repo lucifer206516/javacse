5fd67bfc0d6ecb27eacdd6ba93e4450cc44133b2
lucifer206516 <bharth0@lucifer@gmail.com>
2025-03-15 09:40:27 +0530

    sum

---
+++ ac/sum.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class sum {
+    public static void main(String[] args)
+    {
+        int i=0;
+        Scanner obj=new Scanner(System.in);
+        System.out.println("enter the number of digit");
+        int n=obj.nextInt();
+        int a[]=new int[n];
+        int sum=0;
+        while(i<n){
+            System.out.println("enter the number ");
+             a[i]=obj.nextInt();
+             sum =sum+a[i];
+            i++;
+        }
+       
+        
+        System.out.println("the addition is" +sum);    
+    
+    }
+}
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
